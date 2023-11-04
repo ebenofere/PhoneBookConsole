1110e4a4722a41e569e382339a9e0f4f8d2f4c81
Eben Ofere <ebenezerofere@gmail.com>
2023-11-04 00:08:31 -0400

    update

--- PhoneBookConsole/Program.cs
+++ PhoneBookConsole/Program.cs
@@ -8,29 +8,49 @@ Console.WriteLine("1 Add Contact");
 Console.WriteLine("2 Display contact by number");
 Console.WriteLine("3 View all contacts");
 Console.WriteLine("4 Search for contacts for a given name");
+Console.WriteLine("Press 'x' to exit");
 
 var userInput = Console.ReadLine();
 
 var phoneBook = new PhoneBook();
 
-switch (userInput)
+while (true)
 {
-    case "1":
-        Console.WriteLine("Contact name:");
-        var name = Console.ReadLine();
-        Console.WriteLine("Contact number;");
-        var number = Console.ReadLine();
+    switch (userInput)
+    {
+        case "1":
+            Console.WriteLine("Contact name:");
+            var name = Console.ReadLine();
+            Console.WriteLine("Contact number;");
+            var number = Console.ReadLine();
 
-        var newContact = new Contact(name, number);
-        phoneBook.AddContact(newContact);
+            var newContact = new Contact(name, number);
+            phoneBook.AddContact(newContact);
 
-        break;
-    case "2":
-        break;
-    case "4":
-        break;
-    default:
-        Console.WriteLine("Select valid operation");
-        break;
+            break;
+        case "2":
+            Console.WriteLine("Search for contact by number");
+            var contactNo = Console.ReadLine();
+            phoneBook.DisplayContact(contactNo);
 
+            break;
+        case "3":
+            phoneBook.DisplayAllContact();
+
+            break;
+        case "4":
+            Console.WriteLine("Search for contact by name");
+            var contactName = Console.ReadLine();
+            phoneBook.DisplayMatchingContacts(contactName);
+
+            break;
+        case "x":
+            return;
+        default:
+            Console.WriteLine("Select valid operation");
+            break;
+    }
+
+    Console.WriteLine("Select operation");
+    userInput = Console.ReadLine();
 }
